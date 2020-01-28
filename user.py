0898b5634d16838cd93298e0cabaa35a34af5b2e
admin <admin@onpeeps.com>
2020-01-28 16:12:08 +0300

    server-client

--- user.py
+++ user.py
@@ -3,7 +3,7 @@ import tornado.web
 import client
 import provider
 import json
-
+import requests
 import logging
 logging.basicConfig()
 
@@ -11,6 +11,7 @@ CLIENT_ID = 'abc'
 CLIENT_SECRET = 'xyz'
 SERVER_URI = 'https://localhost:8888'
 REDIRECT_URI = 'https://localhost:8888/oauth2redirect'
+
 
 class MockAuthorizationProvider(provider.AuthorizationProvider):
     """Implement an authorization oauth2lib provider for testing purposes."""
@@ -23,7 +24,7 @@ class MockAuthorizationProvider(provider.AuthorizationProvider):
 
     def validate_scope(self, client_id, scope):
         requested_scopes = scope.split()
-        if client_id == CLIENT_ID :
+        if client_id == CLIENT_ID:
             return True
         return False
 
@@ -31,12 +32,10 @@ class MockAuthorizationProvider(provider.AuthorizationProvider):
         return True
 
     def from_authorization_code(self, client_id, code, scope):
-            return {'session': '12345'}
-       
+        return {'session': '12345'}
 
     def from_refresh_token(self, client_id, refresh_token, scope):
         return {'session': '56789'}
-       
 
     def validate_access(self):
         return True
@@ -56,49 +55,50 @@ class MockAuthorizationProvider(provider.AuthorizationProvider):
         pass
 
 
+myClient = client.Client(client_id=CLIENT_ID,
+                         client_secret=CLIENT_SECRET,
+                         authorization_uri=SERVER_URI + '/auth',
+                         token_uri=SERVER_URI + '/token',
+                         redirect_uri=REDIRECT_URI)
+provider = MockAuthorizationProvider()
+
 
 class MainHandler(tornado.web.RequestHandler):
-    client = client.Client(client_id=CLIENT_ID,
-                    client_secret=CLIENT_SECRET,
-                    authorization_uri=SERVER_URI + '/auth',
-                    token_uri=SERVER_URI + '/token',
-                    redirect_uri=REDIRECT_URI)
-    provider = MockAuthorizationProvider() 
-
-    uri =  client.get_authorization_code_uri()
-    data = provider.get_authorization_code_from_uri(uri)
-    d = data.headers['Location'].split('code=')
-    print(d[1])
-    ser = provider.get_token('authorization_code',
-                  CLIENT_ID,
-                  CLIENT_SECRET,
-                  data.headers['Location'],
-                  d)
-    print(ser)
-    
     def get(self):
-        self.write("""<html>
-    <body>
-        <p>
-            <a href="https://localhost:8888/callback?confirm=1">confirm</a>
-        </p>
-        <p>
-            <a href="https://localhost:8888/callback?confirm=0">deny</a>
-        </p>
-    </body>
-</html>""")
+
+        uri = myClient.get_authorization_code_uri()
+        data = provider.get_authorization_code_from_uri(uri)
+
+        if self.request.uri.startswith("/get_authorization_code"):
+            print(data.headers)
+            if self.get_query_argument("client_id") == CLIENT_ID:
+                self.redirect(data.headers['Location'])
+            else:
+                self.write("acess denied")
+        else:
+            d = data.headers['Location'].split('code=')
+            print(d)
+            ser = provider.get_token('authorization_code',
+                                     CLIENT_ID,
+                                     CLIENT_SECRET,
+                                     SERVER_URI + self.request.uri,
+                                     d)
+
+            print(ser.headers['data'])
+            self.write(ser.headers['data'])
+
 
 class CallbackHandler(tornado.web.RequestHandler):
     def get(self):
         print(self.get_query_argument("confirm"))
-        if self.get_query_argument("confirm") == "1" :
+        if self.get_query_argument("confirm") == "1":
             self.write("""<html>
                             <body>
                                 <p>
                                     AAYA</p>
                             </body>
                         </html>""")
-        else : 
+        else:
             self.write("""<html>
                         <body>
                             <p>
@@ -107,16 +107,45 @@ class CallbackHandler(tornado.web.RequestHandler):
                     </html>""")
 
 
+class ClientHandler(tornado.web.RequestHandler):
+    def get(self):
+
+        if self.request.uri.startswith("/app"):
+            print("....")
+            self.write("""<html>
+                            <body>
+                               <a href="https://localhost:8888/get_authorization_code?client_id=abc">get code</a>
+                            </body>
+                        </html>""")
+        else:
+            self.write("Not found")
+
+
 if __name__ == "__main__":
-    
-    application = tornado.web.Application([
-        (r"/app", MainHandler),
+
+    # Auth server
+    server_application = tornado.web.Application([
+        (r"/get_authorization_code.*", MainHandler),
         (r"/callback.*", CallbackHandler),
+        (r"/oauth2redirect.*", MainHandler),
     ])
-    http_server = tornado.httpserver.HTTPServer(application, ssl_options={
-        "certfile": "/home/sakku/Desktop/localhost.crt",
-        "keyfile": "/home/sakku/Desktop/localhost.key",
+    http_auth_server = tornado.httpserver.HTTPServer(server_application, ssl_options={
+        "certfile": "/Users/imac/Desktop/localhost.crt",
+        "keyfile": "/Users/imac/Desktop/localhost.key",
     })
-    print("https://localhost:8888/app")
-    http_server.listen(8888)
+    print("https://localhost:8888/")
+    http_auth_server.listen(8888)
+
+    #Client :Browser
+    client_application = tornado.web.Application([
+        (r"/app", ClientHandler),
+
+    ])
+    http_client_server = tornado.httpserver.HTTPServer(client_application, ssl_options={
+        "certfile": "/Users/imac/Desktop/localhost.crt",
+        "keyfile": "/Users/imac/Desktop/localhost.key",
+    })
+    print("https://localhost:9999/app")
+
+    http_client_server.listen(9999)
     tornado.ioloop.IOLoop.current().start()
